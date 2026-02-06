ff6c5062f28bf204c082b9421ecd881271780620
David Sanchez <david38sanchez@gmail.com>
2026-02-06 08:19:02 -0500

    Updating utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const capitalize = (str: string): string => {
+  if (!str) return str;
+  return str[0].toUpperCase() + str.slice(1);
+};
