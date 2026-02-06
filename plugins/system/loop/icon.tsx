8531c9e0f6d7e609c9e4abf01ad18b1ce428ef01
David Sanchez <david38sanchez@gmail.com>
2026-02-06 13:15:18 -0500

    Adding new Loop plugin

---
+++ plugins/system/loop/icon.tsx
@@ -0,0 +1,21 @@
+export function LoopIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      aria-label="Loop icon"
+      className={className}
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Loop</title>
+      <path d="M17 2l4 4-4 4" />
+      <path d="M3 11v-1a4 4 0 0 1 4-4h14" />
+      <path d="M7 22l-4-4 4-4" />
+      <path d="M21 13v1a4 4 0 0 1-4 4H3" />
+    </svg>
+  );
+}
